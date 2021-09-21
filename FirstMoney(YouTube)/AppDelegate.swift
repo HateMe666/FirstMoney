72445c464a75179f75fcc19de5c6bb87d6a62e8d
HateMe666 <nikita.kuzmich.11@gmail.com>
2021-09-21 20:58:08 +0300

    update gitiognore

--- FirstMoney(YouTube)/AppDelegate.swift
+++ FirstMoney(YouTube)/AppDelegate.swift
@@ -14,6 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
+    
+    
+    
 
     // MARK: UISceneSession Lifecycle
 
